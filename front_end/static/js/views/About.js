acb42fd399496535ffc1231d4d46ead2ebd8a0a5
lkw303 <lkw303@gmail.com>
2021-01-11 13:14:17 +0800

    change #canvas css property touch-action to none

--- front_end/static/js/views/About.js
+++ front_end/static/js/views/About.js
@@ -13,7 +13,10 @@ export default class extends AbstractView{
            This App was created so we could stop using cones on the ground and criss-crossing our hands to move them around realising that we only have two hands and calling another two people to come move the other four cones and end up with a cluster fuck blocking people from seeing what was displayed.
         </p>
         <p>
-            <a href = "/posts" data-link> View recent posts</a>
+            Visit the github repository.
+            Make a Pull request if you would like to propose some improvements!
+            </br>
+            <a href = "https://github.com/lkw303/TouchRugby" > Github Repository</a>
         </p>
         `;
     }
